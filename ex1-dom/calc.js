4080692a816c3b6801e0388f0142ae6d16c111f2
Ran <lan4250@naver.com>
2023-03-29 00:19:42 +0900

    feat/ 초기화 버튼, 계산하기 버튼 완성, for문안에  빠진 부분 length 추가

--- ex1-dom/calc.js
+++ ex1-dom/calc.js
@@ -5,25 +5,44 @@ window.addEventListener("load", function(){
     const yInput = inputs[1];
     const resetButton = inputs[2];
     const submitButton = inputs[3];
-    const resultSpan = this.document.querySelector("form span : last-child");
+    const resultSpan = document.querySelector("form span:last-child");
 
-    for(let i = 0; i < inputs; ++i){
+    for(let i=0; i<inputs.length; ++i){
         console.log(inputs[i]);
     }
 
     console.log(resultSpan);
 
-    resetButton.onClick = (e) => {
-        xInput = 0;
-        yInput = 0;
+
+    resetButton.onClick = (e)=>{
+        e.preventDefault();
+        xInput.value = 0;
+        yInput.value = 0;
         resultSpan.innerText = 0;
-        
     }; 
 
 
-    submitButton.onClick = (e) => {
-
-
+    submitButton.onClick = (e)=>{
+        e.preventDefault();
+        let x = parseInt(xInput.value);
+        let y = parseInt(yInput.value);
+        let result = x + y;
+        resultSpan.innerText = result;
     };
+
+    xInput.oninput = (e)=>{
+        let x = parseInt(xInput.value);
+        let y = parseInt(yInput.value);
+        let result = x + y;
+        resultSpan.innerText = result;
+    };
+
+    yInput.oninput = (e)=>{
+        let x = parseInt(xInput.value);
+        let y = parseInt(yInput.value);
+        let result = x + y;
+        resultSpan.innerText = result;
+    };
+
 
 });
