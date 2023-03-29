a420a360fb7882c4c717f26cdfd3f543b6656ed6
Ran <lan4250@naver.com>
2023-03-29 11:37:48 +0900

    14강 기존 코드 Vue.js3 으로 바꾸기

--- ex1-vue/calc.js
+++ ex1-vue/calc.js
@@ -1,26 +1,23 @@
-new Vue({
-    el: "#calc-app",
-    data: {
-        x: 11,
-        y: 10,
-        result: 30
+Vue.createApp({
+    data() {
+        return {
+            x: 11,
+            y: 10,
+            result: 30
+        }
     },
-
     methods: {
         resetHandler: function () {
             this.x = 0;
             this.y = 0;
             this.result = 0;
         },
-
         submitHandler() {
             this.result = parseInt(this.x) + parseInt(this.y);
         },
-
         changeHandler() {
             this.result = parseInt(this.x) + parseInt(this.y);
         }
-
     }
-
-});
+})
+.mount("#calc-app");
